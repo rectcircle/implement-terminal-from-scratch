bb9c1bafac9ca2464c55771a155eb8fa19bdb4a2
rectcircle <rectcircle96@gmail.com>
2025-07-20 18:56:37 +0800

    feat: 重构demo目录结构并新增02-xterm-js-on-data demo
    将02-webshell-demo重命名为03-webshell-demo
    新增02-xterm-js-on-data演示项目
    更新vercel项目配置和.gitignore文件
    同步更新各demo的依赖和配置文件

---
+++ demo/02-xterm-js-on-data/src/main.js
@@ -0,0 +1,27 @@
+import './style.css'
+import '../node_modules/@xterm/xterm/css/xterm.css'
+import { Terminal } from '@xterm/xterm'
+
+const terminalASNIEscapeSeqDemo = `随意按键盘观察 xterm.js onData 的输出: `;
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  const terminal = new Terminal();
+  terminal.open(document.querySelector('#terminal'));
+  const xtermJsOnDataCode = document.querySelector('#xterm_js_on_data_code');
+
+  terminal.onData((data) => {
+    xtermJsOnDataCode.textContent += JSON.stringify(data) + "\n";
+  });
+
+  for (const char of terminalASNIEscapeSeqDemo) {
+    terminal.write(char);
+    await sleep(100);
+  }
+
+}
+
+main();
